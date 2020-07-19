87ff9d6cf3ecdd2d43a3c27d7e92151a40d09aca
Alex Teisheira <fukirx@gmail.com>
2020-07-20 00:01:14 +0300

    gitignore

--- src/services/testifyApi.js
+++ src/services/testifyApi.js
@@ -1,29 +1,28 @@
 export default class TestifyApi {
+  //_base = 'http://192.168.1.16:3000'
+  _base = "http://localhost:3000";
 
-    _base = 'http://192.168.1.16:3000'
-    //__base = 'http://localhost:3000'
+  get = async url => {
+    return fetch(this._base + url, {
+      method: "get",
+      credentials: "include",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    });
+  };
 
-    get = async(url) => {
-
-      return fetch(this._base + url, {
-          method: 'get',
-          credentials: 'include',
-          headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-          }
-      })
-  }
-
-    post = async (body, url) => {
-         return fetch(this._base + url, {
-            method: 'post',
-            credentials: 'include',
-            headers: {
-              'Accept': 'application/json, text/plain, */*',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        })
-    }
+  post = async (body, url) => {
+    console.log(this._base + url);
+    return fetch(this._base + url, {
+      method: "post",
+      credentials: "include",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    });
+  };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,112 +1,110 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = (env = {}) => {
+  const { mode = "development" } = env;
 
-    const { mode = 'development'} = env;
+  const isProd = mode === "production";
+  const isDev = mode === "development";
 
-    const isProd = mode === 'production'
-    const isDev = mode === 'development'
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : "style-loader",
+      "css-loader"
+    ];
+  };
 
-    const getStyleLoaders = () => {
-        return [
-            isProd? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader'
-        ]
-    }
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: "Testify",
+        template: "public/index.html"
+      })
+    ];
 
-    const getPlugins = () => {
-        const plugins = [
-            new HtmlWebpackPlugin({
-                title: 'Testify',
-                template: 'public/index.html'
-            }),
-        ]
+    if (isProd)
+      plugins.push(new MiniCssExtractPlugin({ filename: "main-[hash:8].css" }));
 
-        if (isProd) plugins.push(new MiniCssExtractPlugin({filename: 'main-[hash:8].css'}))
+    return plugins;
+  };
 
-        return plugins
-    }
+  return {
+    mode: isProd ? "production" : isDev && "development",
 
-    return {
-        mode: isProd? 'production': isDev && 'development',
+    output: {
+      filename: isProd ? "main-[hash:8].js" : undefined,
+      publicPath: "/"
+    },
 
-        output: {
-            filename: isProd? 'main-[hash:8].js' : undefined,
-            publicPath: '/'
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader"
+            }
+          ]
         },
-
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'babel-loader'
-                        }
-                    ]
-                },
-                //loading images
-                {
-                    test: /\.(png|jpg|gif|ico|jpeg)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                outputPath: 'images',
-                                name: '[name]-[sha1:hash:7].[ext]'
-                            }
-                        }
-                    ]
-                },
-                // loading fonts
-                {
-                    test: /\.(ttf|otf|eof|woff|woff2)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                outputPath: 'fonts',
-                                name: '[name].[ext]'
-                            }
-                        }
-                    ]
-                },
-                //loading css
-                {
-                    test: /\.css$/,
-                    use: getStyleLoaders()
-                },
-                //loading sass
-                {
-                    test: /\.s[ca]ss$/,
-                    use: [
-                        ...getStyleLoaders(), 
-                        'sass-loader',
-                        {
-                            loader: 'sass-resources-loader',
-                            options: {
-                              resources: ['./src/resources/vars.sass' ]
-                            },
-                          } 
-                    ]
-                }
-
-
-            ]
+        //loading images
+        {
+          test: /\.(png|jpg|gif|ico|jpeg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "images",
+                name: "[name]-[sha1:hash:7].[ext]"
+              }
+            }
+          ]
         },
-
-        plugins: getPlugins(),
-
-        devServer: {
-            host: '0.0.0.0',
-            disableHostCheck: true,
-            open: true,
-            port: 8000,
-            historyApiFallback: true,
-            contentBase: './',
-            hot: true,
-            openPage: 'test/create'
+        // loading fonts
+        {
+          test: /\.(ttf|otf|eof|woff|woff2)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "fonts",
+                name: "[name].[ext]"
+              }
+            }
+          ]
+        },
+        //loading css
+        {
+          test: /\.css$/,
+          use: getStyleLoaders()
+        },
+        //loading sass
+        {
+          test: /\.s[ca]ss$/,
+          use: [
+            ...getStyleLoaders(),
+            "sass-loader",
+            {
+              loader: "sass-resources-loader",
+              options: {
+                resources: ["./src/resources/vars.sass"]
+              }
+            }
+          ]
         }
+      ]
+    },
+
+    plugins: getPlugins(),
+
+    devServer: {
+      //host: '0.0.0.0',
+      //disableHostCheck: true,
+      open: true,
+      port: 8000,
+      historyApiFallback: true,
+      contentBase: "./",
+      hot: true,
+      openPage: "test/create"
     }
-}
+  };
+};
