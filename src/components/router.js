caabfc3adda09b3c5d782e331161ffad6b54555e
Alex Teisheira <fukirx@gmail.com>
2021-06-01 21:56:20 +0300

    homepage added

--- src/components/router.js
+++ src/components/router.js
@@ -4,9 +4,11 @@ import { Route, Switch } from "react-router-dom";
 import TestCreator from "components/CreatorApp/TestCreator";
 import TestProcess from "components/ExamApp/TestProcess";
 import TestResults from "components/ExamApp/TestResults";
+import Home from "components/home";
 
 const Routing = () => (
   <Switch>
+    <Route path="/" exact component={Home} />
     <Route path="/test/create" component={TestCreator} />
     <Route path="/test/:id" component={TestProcess} />
     <Route path="/results/:id" component={TestResults} />
